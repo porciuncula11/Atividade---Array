34ffcbe88c8fcf78090764abd1941e7faa1955d4
porciuncula11 <108681942+porciuncula11@users.noreply.github.com>
2023-03-27 12:28:56 -0300

    Add files via upload

---
+++ Array - Atividade/script.js
@@ -0,0 +1,11 @@
+const input = document.getElementById('input');
+const adicionar = document.getElementById('adicionar');
+const ordenar = document.getElementById('ordenar');
+
+let arrya = []
+
+adicionar.addEventListener('click', function(){
+    Array.push(input.value);
+    console.log(array)
+})
+
